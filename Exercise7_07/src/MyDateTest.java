f621cf44417dba71bd656c5a637880562f1aa7c8
Paul <bujor.paul2000@gmail.com>
2019-09-26 12:25:49 +0200

    Exercise 8 a-m

--- Exercise7_07/src/MyDateTest.java
+++ Exercise7_07/src/MyDateTest.java
@@ -2,9 +2,37 @@ public class MyDateTest
 {
   public static void main(String[] args)
   {
-    MyDate date = new MyDate(19, 6, 2000);
+    MyDate date = new MyDate(29, 10, 2001);
     System.out.println(date);
-    date.set(12,12,2012);
+
+    MyDate date2 = new MyDate(6,12,2006);
+    System.out.println(date2);
+
+    date2 = date.copy();
+    System.out.println(date2);
+
+
+
+    /*
     System.out.println(date);
+    date.stepForwardOneDay();
+    System.out.println(date);
+    date.stepForwardOneDay();
+    System.out.println(date);
+
+    date.set(30,12,2016);
+    System.out.println(date);
+    date.stepForwardOneDay();
+    System.out.println(date);
+    date.stepForwardOneDay();
+    System.out.println(date);
+
+    date.set(27,2,2019);
+    System.out.println(date);
+    date.stepForwardOneDay();
+    System.out.println(date);
+    date.stepForwardOneDay();
+    System.out.println(date);
+    */
   }
 }
